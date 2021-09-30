943cfe9ca0d6b487512b5cb6c88912eeea96c3ab
JSimple <josephinemayasimple@gmail.com>
2021-09-30 18:03:02 -0400

    sequelize essentials, syncing our models

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
+const wikiRouter = require('./routes/wiki');
+const usersRouter = require('./routes/users');
 
 const app = express();
 
 const layout = require("./views/layout");
 const { db, Page, User } = require('./models');
 
+
 app.use(express.urlencoded({extended: false}));
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
+app.use('/wiki', wikiRouter);
+app.use('/users', usersRouter);
 
 app.get("/", (req, res) => {
     console.log("Hello world")
@@ -27,6 +32,8 @@ const init = async () => {
       console.log(`Server is listening on port ${PORT}!`);
     });
   }
+
 init();
+
 const PORT = 3000;
 
